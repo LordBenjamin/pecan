98797bd0c5719e22f936a1c33222fcbcca40a7fc
Ben Owen <LordBenjamin@users.noreply.github.com>
2021-07-21 18:36:23 +0100

    Remove unused usings.

--- src/Pecan/WebServerBuilder.cs
+++ src/Pecan/WebServerBuilder.cs
@@ -1,10 +1,8 @@
 ﻿using Pecan.Logging;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Pecan
