e9e24ede1a7fdc4fde6619036b2029941467ed03
Ben Owen <LordBenjamin@users.noreply.github.com>
2022-04-02 20:30:49 +0100

    * Logging improvements (including per-request correlation IDs) * Improved exception handling * Factored out buffered string write into a separate method

--- src/Pecan/WebServer.cs
+++ src/Pecan/WebServer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -56,64 +57,94 @@ namespace Pecan
 
         private async Task MainLoop(CancellationToken ct)
         {
-            logger?.Log($"Started");
+            logger?.Log("Pecan Web Server started");
 
+            ulong requestId = 0;
             while (httpListener.IsListening && !ct.IsCancellationRequested)
             {
-                logger?.Log($"Waiting for request");
+                logger?.Log("Waiting for request");
                 var context = await httpListener.GetContextAsync()
                     .ConfigureAwait(false);
+
+                requestId = unchecked(requestId + 1); // it's okay to roll over this value, it's just for log correlation
+                logger?.Log($"[{requestId}] {context.Request.HttpMethod} {context.Request.RawUrl}");
 
                 Stopwatch sw = Stopwatch.StartNew();
 
                 var key = (context.Request.Url.AbsolutePath.ToUpperInvariant(), context.Request.HttpMethod.ToUpperInvariant());
 
-                logger?.Log($"{context.Request.HttpMethod} {context.Request.RawUrl}");
-
-                if (matchers.TryGetValue(key, out var handler))
-                {
-                    object response = await handler(context)
-                        .ConfigureAwait(false);
-
-                    if (response is VoidHolder)
-                    {
-                        // Void return, nothing to flush
-                        goto completeRequest;
-                    }
-                    else if (response is string str)
-                    {
-                        byte[] buffer = ArrayPool<byte>.Shared.Rent(str.Length);
-
-                        try
-                        {
-                            int count = Encoding.UTF8.GetBytes(str, buffer);
-
-                            await context.Response.OutputStream.WriteAsync(buffer, 0, count)
-                                .ConfigureAwait(false);
-                        }
-                        catch (Exception ex)
-                        {
-                            logger?.Log($"Error processing request: {ex}");
-                            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                        }
-                        finally
-                        {
-                            ArrayPool<byte>.Shared.Return(buffer);
-                        }
-                    }
-                }
-                else
+                if (!matchers.TryGetValue(key, out var handler))
                 {
                     context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+                    goto completeRequest;
+                }
+
+                object response = VoidHolder.AsObject;
+                try
+                {
+                    response = await handler(context)
+                        .ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    logger?.Log($"[{requestId}] Error processing request: {ex}");
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                }
+
+                if (response is VoidHolder)
+                {
+                    // Void return, nothing to flush
+                    goto completeRequest;
+                }
+                else if (response is string str)
+                {
+                    context.Response.ContentLength64 = str.Length;
+                    await WriteStringBuffered(context.Response.OutputStream, str)
+                        .ConfigureAwait(false);
                 }
 
             completeRequest:
                 context.Response.Close();
 
-                logger?.Log($"{context.Response.StatusCode} ({sw.ElapsedMilliseconds:n0} ms)");
+                logger?.Log($"[{requestId}] {context.Response.StatusCode} ({sw.ElapsedMilliseconds:n0} ms)");
+            }
+
+            if (ct.IsCancellationRequested)
+            {
+                httpListener.Stop();
             }
 
             logger?.Log($"Stopped");
+        }
+
+        private Task WriteStringBuffered(Stream stream, string str, int bufferSize = 4096)
+        {
+            byte[] buffer = ArrayPool<byte>.Shared.Rent(str.Length);
+
+            try
+            {
+                return WriteStringBuffered(stream, str, buffer);
+            }
+            finally
+            {
+                ArrayPool<byte>.Shared.Return(buffer);
+            }
+        }
+
+        private async Task WriteStringBuffered(Stream stream, string str, byte[] buffer)
+        {
+            int readIndex = 0;
+            int bytesRead;
+
+            do
+            {
+                int bytesToRead = Math.Min(str.Length - readIndex, buffer.Length);
+                bytesRead = Encoding.UTF8.GetBytes(str, readIndex, bytesToRead, buffer, 0);
+                readIndex += bytesRead;
+
+                await stream.WriteAsync(buffer, 0, bytesRead)
+                    .ConfigureAwait(false);
+            } while (bytesRead > 0);
         }
 
         public void Stop()
